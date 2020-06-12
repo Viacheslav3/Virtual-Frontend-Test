9ee8aba069f5675c3af5b876688d5733ee297743
Viacheslav <slavkodrozd111@gmail.com>
2020-06-12 16:36:09 +0200

    Add all files

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+/* Here goes your JS code */
+
+    // Jquery for 
+$( "#button" ).on( "click", function() {
+    $( "#show-popup-form").text( "Thank you!" );
+    $( "#show-popup-form" )
+      .hide()
+      .fadeOut( 3000 )
+      .fadeIn( 1000 );
+  });
+
+  var ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+
+  function validate(form){
+  var email = form.email.value;
+  var password = form.password.value;
+  var errors = [];
+
+   if (!ck_email.test(email)) {
+    errors[errors.length] = "You must enter a valid email address.";
+   }
+
+   if (password=='') {
+    errors[errors.length] = "You must enter the password ";
+   }
+
+   if (errors.length > 0) {        
+    reportErrors(errors);
+    return false;
+   }
+    return true;
+  }
+
+ function reportErrors(errors){
+   var msg = "Please Enter Valide Data...\n";
+   for (var i = 0; i<errors.length; i++) {
+   var numError = i + 1;
+    msg += "\n" + numError + ". " + errors[i];
+  }
+   alert(msg);
+  }
